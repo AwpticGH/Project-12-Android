84d28be36aea3bfdd49d537fe180105fa5a64636
Rafi Fajar Sulaiman <rafihariyadi01@gmail.com>
2023-07-04 10:08:06 +0700

    v:1.0.0 \ Change environment to use real firebase

--- app/src/main/java/g10/manga/comicable/backend/app/config/firebase/AuthConfig.java
+++ app/src/main/java/g10/manga/comicable/backend/app/config/firebase/AuthConfig.java
@@ -7,9 +7,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class AuthConfig {
 
     public static FirebaseAuth getFirebaseAuth() {
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        auth.useEmulator("10.0.2.2", 9099);
-        return auth;
+        return FirebaseAuth.getInstance();
     }
 
     public static FirebaseUser getFirebaseUser() {
