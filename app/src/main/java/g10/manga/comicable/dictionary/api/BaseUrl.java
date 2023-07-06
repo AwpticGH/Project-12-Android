cf9f5c7069fdefd5d49ce560b66f32c0a67998b8
Rafi Fajar Sulaiman <rafihariyadi01@gmail.com>
2023-07-07 04:20:30 +0700

    Changed BaseUrl For Api

--- app/src/main/java/g10/manga/comicable/dictionary/api/BaseUrl.java
+++ app/src/main/java/g10/manga/comicable/dictionary/api/BaseUrl.java
@@ -2,6 +2,6 @@ package g10.manga.comicable.dictionary.api;
 
 public class BaseUrl {
 
-    public final static String URL = "https://g2airline-389604.et.r.appspot.com";
+    public final static String URL = "https://optimum-agent-387504.et.r.appspot.com";
 
 }
